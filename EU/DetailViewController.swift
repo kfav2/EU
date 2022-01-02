470b00ec1bb483830d63889f40834664104602ae
KFAV <kofava14@gmail.com>
2022-01-02 10:51:18 -0800

    Ch. 5 3 Working with Multiple View Controllers + Segues

---
+++ EU/DetailViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DetailViewController.swift
+//  EU
+//
+//  Created by Korlin Favara on 1/2/22.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var countryField: UITextField!
+    @IBOutlet weak var capitalField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        
+            // code is asking the presentingViewController if it is a UINavigationController.
+            // UINavigationControllers present modally - have to dismiss
+            // otherwise view controller is not going through navigation controller - have to pop
+            // it should be noted these are all in a higher level navigatio controller
+            // isPresentingInAddMode becomes true or false
+            let isPresentingInAddMode = presentingViewController is UINavigationController
+            if isPresentingInAddMode {
+                dismiss(animated: true, completion: nil)
+            } else {
+                // although at this point you would not be in a UINavigationController, popViewController pops the top view controller from the stack
+                navigationController?.popViewController(animated: true)
+            }
+    }
+    
+
+}
+  
