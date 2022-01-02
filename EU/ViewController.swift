7c2333429cc9bd1fb079b1cf0ff937fc4e8272c2
KFAV <kofava14@gmail.com>
2022-01-01 18:07:19 -0800

    Ch. 5 2 Setting up a TableView

--- EU/ViewController.swift
+++ EU/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let EUArray = ["Austria",
+                   "Belgium",
+                   "Bulgaria",
+                   "Croatia",
+                   "Cyprus",
+                   "Czechia",
+                   "Denmark",
+                   "Estonia",
+                   "Finland",
+                   "France",
+                   "Germany",
+                   "Greece",
+                   "Hungary",
+                   "Ireland",
+                   "Italy",
+                   "Latvia",
+                   "Lithuania",
+                   "Luxembourg",
+                   "Malta",
+                   "Netherlands",
+                   "Poland",
+                   "Portugal",
+                   "Romania",
+                   "Slovakia",
+                   "Slovenia",
+                   "Spain",
+                   "Sweden",
+                   "United Kingdom"]
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 
 }
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    // how many cells are in UITable, returns Int
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        EUArray.count
+    }
+    
+    // formatting cells so that they are "re-used", iOS only uses a defined amount of cells on a page. e.g. if table has 1000 elements, when you scroll the cells move and are reused
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = EUArray[indexPath.row]
+        return cell
+    }
+    
+}
